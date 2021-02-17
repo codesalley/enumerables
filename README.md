aa81085083edbf9ce6b12fd5b98baa3f6dbadf4f
juxsalley <juxsalley@gmail.com>
2021-02-17 17:18:01 +0000

    add test setup using rspec

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# enumerables
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Enumerables
+In this project, we have implemented our version of Ruby’s enumerable methods.
+
+## Enumerable Methods List:
+
+- my_each
+- my_each_with_index
+- my_select
+- my_all?
+- my_any?
+- my_none?
+- my_count
+- my_map
+- my_inject
+- multiply_els
+
+<img align="right" alt="" height="300px" src="./screenshot.png" />
+
+## Built With Ruby
+
+![Ruby](http://img.shields.io/badge/-Ruby-3776AB?style=flat-square&logo=ruby&logoColor=red)
+
+## Dependecies
+
+- Ruby 3.0 (or higher)
+
+## Run Program
+
+__Clone the repo to your local machine using the terminal__:
+```
+$ git clone https://github.com/juxsalley/enumerables.git
+```
+
+1. Navigate into the project directory 
+   - `cd enumerables`
+   - ``cd lib``
+2. Run the project `ruby enumerables.rb`
+
+## Run Tests
+
+- After cloning repo run ```bundle install``` to install gems
+- ```bundle exec rspec``` to run test.
+
+
+
+## Author
+
+### Author 
+
+👤 **Salley**
+
+- GitHub: [@JuxSalley](https://github.com/juxsalley)
+- Twitter: [__salley](https://twitter.com/__salley)
+- LinkedIn: [Iddrisu salley](https://www.linkedin.com/in/dev-salley/)
+
+
+## Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/CacheMeGifYouCan/Enumerables-Project/issues)
+
+## 📝 License
+
+This project is [MIT](LICENSE) licensed.
---
+++ spec/enumerables_spec.rb
@@ -0,0 +1,87 @@
+require 'enumerables'
+
+puts '\nNOTE: All test must be passed'
+
+describe "Enumerable" do
+  let(:my_enumerables) { Enumerable.new }
+  describe "#my_each" do
+    it 'my_each must be an Enumerator ' do
+      expect([1, 2, 3].my_each.class == Enumerator).to eq(true)
+    end
+    it 'when a block is given when self is an array returns the array itself after calling the given block once for each element in self' do
+      expect([1, 2, 3, 4, 5].each(&proc { |n| n % 2 }) == [1, 2, 3, 4, 5].my_each(&proc { |n| n % 2 })).not_to eq(false)
+    end
+  end
+  describe '#my_each_index' do
+    it 'my_each_with_index must be an Enumerator ' do
+      expect([1, 2, 3].my_each_with_index.class == Enumerator).to eq(true)
+    end
+    it 'my_each_with_index when a block is given when self is a range' do
+      concat1 = ''
+      (1..4).each_with_index(&proc { |n, i| concat1 += 'n: ' + n.to_s + ', i: ' + i.to_s + '+++' })
+      concat2 = ''
+      (1..4).my_each_with_index(&proc { |n, i| concat2 += 'n: ' + n.to_s + ', i: ' + i.to_s + '+++' })
+
+      expect(concat1 == concat2).to eq(true)
+    end
+
+  end
+  describe '#my_select' do
+    it 'returns an enumerator if no block is given' do
+      expect([1, 2, 3, 4].my_select.class == Enumerator).not_to eq(false)
+    end
+    it 'ruturn array of all selected elements' do
+      expect([1, 2, 3, 4].my_select { |ele| ele > 2 }).to eq([3, 4])
+    end
+  end
+  describe '#my_count' do
+    it 'Returns the length of the given object' do
+      expect([1,2,3,2,4,6,7,4,3,'2'].my_count).to eq(10)
+    end
+  end
+  describe '#my_all?' do
+    it 'when a Regex is passed as an argument returns true if all of the collection matches the Regex ' do
+      expect(['car', 'cat'].all?(/a/) == ['car', 'cat'].my_all?(/a/)).not_to eq(false)
+
+    end
+    it 'when a class is passed as an argument returns true if all of the collection is a member of such class::Numeric' do
+      expect([1, -2, 3.4].all?(Numeric) == [1, -2, 3.4].my_all?(Numeric)).to eq(true)
+    end
+  end
+  describe '#my_any?' do
+    it 'when no block or argument is given returns false if at least one of the collection is not true' do
+      expect([false, 0].any? == [false, 0].my_any?).to eq(true)
+    end
+    it 'when an array is given with a block, it should perform the function on every element of the array' do
+      expect([2, 3, 4, 4, 4, 3, 2, 2, 1].my_any? { |ele| ele.odd? }).to eq(true)
+    end
+  end
+  describe '#my_none? ' do
+    it 'returns false if the block ever returns true for all elements::range' do
+      expect((1..3).none?(&proc { |num| num % 2 == 0 }) == (1..3).my_none?(&proc { |num| num % 2 == 0 })).to eq(true)
+
+    end
+    it ' when no block or argument is given returns false only if one of the collection members is true' do
+      expect([false, nil, []].none? == [false, nil, []].my_none?).to eq(true)
+
+    end
+
+  end
+  describe '#my_map' do
+    it 'should returns an Enumerator if no block is given' do
+      expect([1, 2, 3].map.class == [1, 2, 3].my_map.class).to eq(true)
+    end
+    it 'when an array is given with a block, it should perform the function on every element of the array ' do
+      expect([1, 2, 3].my_map { |ele| ele * 3 }).to eq([3, 6, 9])
+    end
+  end
+  describe '#my_inject' do
+    it 'should searches for the longest word in an array of strings ' do
+      expect(['to', 'four', 'tri'].my_inject(&proc { |a, b| [a, b].max_by { |m| m.length } }) ==
+               ['to', 'four', 'tri'].inject(&proc { |a, b| [a, b].max_by { |m| m.length } })).to eq(true )
+    end
+    it 'when a block is given without an initail value combines all elements of enum by applying a binary operation, specified by a block::range' do
+      expect((1..3).inject(&proc{|total, num| total*num}) == (1..3).my_inject(&proc{|total, num| total*num})).not_to eq(false )
+    end
+  end
+end
